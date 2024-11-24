075e0c926e3e77f9b1bb135bb8fb0b2c37f9af10
ZumTheZazaKing <zahidi85543@gmail.com>
2024-11-24 18:13:46 +0800

    Now stores highest score and sets new one on highest defeat

--- fe/src/components/game/End.jsx
+++ fe/src/components/game/End.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../../Context";
 import { useNavigate } from "react-router-dom";
 
@@ -7,16 +7,29 @@ const End = () => {
     const { state, dispatch } = useContext(Context)
     const navigate = useNavigate()
 
+    const [highest,setHighest] = useState(localStorage.getItem("zum-word-bomb-highest") || 0)
+    const [newHigh,setNewHigh] = useState(false)
+
+    useEffect(() => {
+        if(state.score > highest){
+            setHighest(state.score)
+            localStorage.setItem('zum-word-bomb-highest', state.score)
+            setNewHigh(true)
+        }
+    },[])
+
     const toMainMenu = () => {
         dispatch({ type: "exit_game" })
         navigate("/")
     }
 
     return (
-        <div className="w-screen h-screen flex flex-col gap-5 items-center justify-center text-white">
+        <div className="w-screen h-screen flex flex-col gap-6 items-center justify-center text-white">
             <h2 className="text-2xl font-semibold ">GAME OVER</h2>
-            <div>
+            <div className="text-center text-lg">
+                {newHigh && <p className="text-yellow-500">NEW HIGHEST!</p>}
                 <p>Score: {state.score}</p>
+                <p>Highest Score: {highest}</p>
             </div>
             <div className="flex flex-col items-center gap-3">
                 <button
